5936999de3d80a2ee54f368827349ba16c7aad4c
desco10 <daniel1041esk.arb@gmail.com>
2025-12-22 15:14:45 -0500

    conectadonuevamysql

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,13 +5,19 @@ const pool = mysql.createPool({
   user: "root",
   password: "",
   database: "proveedordb",
+  port: 3307,               // ← CAMBIO ÚNICO Y CLAVE
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
 });
 
 pool.getConnection()
-  .then(() => console.log("Conexión a MySQL exitosa"))
-  .catch(err => console.error("Error conexión MySQL:", err));
+  .then(conn => {
+    console.log("Conexión a MySQL exitosa");
+    conn.release();
+  })
+  .catch(err => {
+    console.error("Error conexión MySQL:", err);
+  });
 
 module.exports = pool;
